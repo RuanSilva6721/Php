4c76545313320da5c63b272a9c65df1c64a33c1f
RuanFelipe <ruansilva6721@gmail.com>
2022-09-05 11:38:47 -0300

    adicionando estrutura de pasta ao Projeto 2

---
+++ Projeto2_Agenda_Ruan/index.php
@@ -0,0 +1,22 @@
+<?php
+include_once "config/url.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous" />
+  <!-- FONT AWESOME -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+  <!-- CSS -->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    <title>Agenda do Ruan Felipe</title>
+
+</head>
+<body>
+<script src="https://kit.fontawesome.com/8106cb0d7e.js" crossorigin="anonymous"></script> 
+</body>
+</html>

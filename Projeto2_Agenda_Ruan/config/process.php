756de3f45082b74bcd9eca34677e9b25b9c14369
RuanFelipe <ruansilva6721@gmail.com>
2022-09-09 12:10:21 -0300

    criando a página de adicionar contato

--- Projeto2_Agenda_Ruan/config/process.php
+++ Projeto2_Agenda_Ruan/config/process.php
@@ -6,34 +6,51 @@
     include_once "connection.php";
     include_once "url.php";
 
-    $id;
-
-    if(!empty($_GET)){
-        $id = $_GET["id"];
-    }
-    //Retorna o dado
-    if(!empty($id)){
-        $query = "SELECT * FROM contacts WHERE id= :id";
-
-        $stmt = $conn->prepare($query);
-
-        $stmt->bindParam(":id", $id);
-        $stmt->execute();
+    $data = $_POST;
+//modificações de dados no banco
+    if(!empty($data)){
+        print_r($data); exit;
         
-        $contact = $stmt->fetct();
+
+
+//seleção de dados
     }else{
+        $id;
 
-    //retorna todos os contatos
-    $contacts = [];
-
-    $query = "SELECT * FROM contacts";
-
-    $stmt = $conn->prepare($query);
-
-    $stmt->execute();
-
-    $contacts = $stmt->fetchAll();
+        if(!empty($_GET)){
+            $id = $_GET["id"];
+        }
+        //Retorna o dado
+        if(!empty($id)){
+            $query = "SELECT * FROM contacts WHERE id= :id";
+    
+            $stmt = $conn->prepare($query);
+    
+            $stmt->bindParam(":id", $id);
+            $stmt->execute();
+            
+            $contact = $stmt->fetch();
+        }else{
+    
+        //retorna todos os contatos
+        $contacts = [];
+    
+        $query = "SELECT * FROM contacts";
+    
+        $stmt = $conn->prepare($query);
+    
+        $stmt->execute();
+    
+        $contacts = $stmt->fetchAll();
+    
+        }
 
     }
+
+
+
+
+
+ 
 
 ?>
--- Projeto2_Agenda_Ruan/create.php
+++ Projeto2_Agenda_Ruan/create.php
@@ -1,6 +1,28 @@
 <?php
 include_once "templates/header.php";
 ?>
-<h1>Contato</h1>
+    <div class="container">
+    <?php include_once("templates/backbtn.html"); ?>
+        <h1 id="main-title">Criar Contato</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="name">Nome do contato: </label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o seu nome" required>
+            </div>
+            <div class="form-group">
+                <label for="name">Telefone do contato: </label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o seu telefone" required>
+            </div>
+            <div class="form-group">
+                <label for="name">Observações: </label>
+                <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Insira as observações" rows="5"></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Cadastrar</button>
+
+        </form>
+    </div>
+
 
 <?php include_once "templates/footer.php"; ?>
